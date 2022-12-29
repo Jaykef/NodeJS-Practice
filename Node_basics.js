9aaf112b69605383d0e3d27e77b504b6b6efefd7
Jaward Sesay <jkef96@icloud.com>
2022-12-29 21:23:39 +0800

    Update Node_basics.js

--- Node_basics.js
+++ Node_basics.js
@@ -13,7 +13,7 @@ fs.readFile('/Users/Jaykef/test.txt', 'utf8', (err, data) => {
 const fs = require('fs'); 
 
 try {
-  const data = fs.readFileSync('/Users/Jaykef/test.txt', 'utf8');
+  const data = fs.readFileSync('/Users/jaykef/test.txt', 'utf8');
   console.log(data);
 } catch (err) {
   console.error(err);
@@ -24,13 +24,26 @@ const fs = require('fs/promises');
 
 async function example() {
   try {
-    const data = await fs.readFile('/Users/Jaykef/test.txt', { encoding: 'utf8' });
+    const data = await fs.readFile('/Users/jaykef/test.txt', { encoding: 'utf8' });
     console.log(data);
   } catch (err) {
     console.log(err);
   }
 }
 example();
+
+// writing files - writeFile()
+const fs = require('fs');
+
+const content = 'Some content!';
+
+fs.writeFile('/Users/jaykef/test.txt', content, err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
+});
+
 
 
 
