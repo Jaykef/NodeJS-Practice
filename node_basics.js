507ca78b65f03b05f0dce15926ae453e61a327cc
Jaward Sesay <jkef96@icloud.com>
2022-12-29 21:46:32 +0800

    Update node_basics.js

--- node_basics.js
+++ node_basics.js
@@ -122,6 +122,18 @@ fs.rmdir(dir, err => {
   console.log(`${dir} is deleted!`);
 });
 
+// 12. accept input from terminal 
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question(`What's your name?`, name => {
+  console.log(`Hi ${name}!`);
+  readline.close();
+});
+
+
 
 
 
