e3c6374d62f7c9c124babcf8b3ca8aaf39a1c25f
Jaward Sesay <jkef96@icloud.com>
2022-12-29 21:24:38 +0800

    Update Node_basics.js

--- Node_basics.js
+++ Node_basics.js
@@ -44,6 +44,18 @@ fs.writeFile('/Users/jaykef/test.txt', content, err => {
   // file written successfully
 });
 
+// writing files synchronously - writeFileSync()
+const fs = require('fs');
+
+const content = 'Some content!';
+
+try {
+  fs.writeFileSync('/Users/joe/test.txt', content);
+  // file written successfully
+} catch (err) {
+  console.error(err);
+}
+
 
 
 
